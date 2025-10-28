d6e42a13d15a7e582a3dcda257a42e8e35b39e8b
SolomonFirferYU <sfirfer@mail.yu.edu>
2025-10-28 12:18:14 -0400

    added chunck-based generation

--- terrain.gd
+++ terrain.gd
@@ -1,22 +1,34 @@
 extends Node
 
+@export var chunck_size := 64
+@export var resolution := 64
+@export var noise : FastNoiseLite
+
+const PRECED_TERRAIN = preload("uid://dvthq2uk78b2m")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	generate_terrain()
+	for x in range(-5,5):
+		for y in range(-5,5):
+			generate_terrain(Vector2(x,y))
 	pass # Replace with function body.
 
-func generate_terrain():
-	var st = SurfaceTool.new()
-	st.begin(Mesh.PRIMITIVE_TRIANGLES)
-	st.set_color(Color(1, 0, 0))
-	st.set_uv(Vector2(0,0))
-	st.add_vertex(Vector3(0, 0, 0))
-	st.add_vertex(Vector3(0, -3, 0))
-	st.add_vertex(Vector3(0, -3, -3))
+func generate_terrain(chunck:Vector2):
+	var x_offset := chunck.x * chunck_size
+	var y_offset := chunck.y * chunck_size
 	
-	var arrm = st.commit()
-	var m = MeshInstance3D.new()
-	m.mesh = arrm
-	add_child(m)
+	#Instantiate the chunck and set size and position
+	var new_chunck = PRECED_TERRAIN.instantiate() as MeshInstance3D
+	new_chunck.size_x = chunck_size
+	new_chunck.size_y = chunck_size
+	new_chunck.resolution = resolution
+	new_chunck.position = Vector3(x_offset,-64,y_offset)
+	
+	#Offset the terrain of the chunck
+	var noise_instance := noise.duplicate()
+	noise_instance.offset = Vector3(x_offset,y_offset,0)
+	new_chunck.noise = noise_instance
+	
+	
+	add_child(new_chunck)
 	print("Generated Terrain")
