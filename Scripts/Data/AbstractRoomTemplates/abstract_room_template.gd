715019e8a16ee2c763d199f529db7a5291adbdea
Solomon Firfer <theepicsuper212@gmail.com>
2025-11-10 15:38:15 -0500

    rethoyght and removed some things

--- Scripts/Data/AbstractRoomTemplates/abstract_room_template.gd
+++ Scripts/Data/AbstractRoomTemplates/abstract_room_template.gd
@@ -3,4 +3,3 @@ class_name AbstractRoomTemplate
 
 @export var min_scale: Vector3
 @export var max_scale: Vector3
-@export var weight: float
--- Scripts/Generation/Unpackers/AbstractBuildings/abstract_buildings_box_compact_unpacker.gd
+++ Scripts/Generation/Unpackers/AbstractBuildings/abstract_buildings_box_compact_unpacker.gd
@@ -1,40 +1,9 @@
 extends Unpacker
 
 @export var models: Array[AbstractRoomTemplate]
-@export var area_min := 100.0
-@export var area_max := 1000.0
-
-# Variable to store the accumulated weights of the models
-var models_max_waights = calculate_models_max_weights()
-
-# Variable to store all boxes placed
-var current_boxes = []
-
-func calculate_models_max_weights():
-	var result = []
-	var current_weight = 0.0
-	for model in models:
-		current_weight += model.weight
-		result.append(current_weight)
-	return result
-	
-func pick_template(weight:float):
-	for i in range(len(models_max_waights)):
-		if weight < models_max_waights[i]:
-			return models[i]
-	return models[-1]
-
-func pick_next_template(rng:RandomNumberGenerator) -> AbstractRoomTemplate:
-	var chosen_weight = rng.randf_range(0,models_max_waights[-1])
-	var template_to_pick = pick_next_template(chosen_weight) as AbstractRoomTemplate
-	return template_to_pick
 
 func unpack() -> Array[Unpacker]:
 	var rng := RandomNumberGenerator.new()
 	rng.seed = seed
 	
-	var final_area = rng.randf_range(area_min,area_max)
-	var current_area = 0
-	while current_area < final_area:
-		var next_template = pick_next_template(rng)
 	return []
