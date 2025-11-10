4d2aebea2d21552242e6762a5aaec00ab0888dbe
Solomon Firfer <theepicsuper212@gmail.com>
2025-11-10 16:06:56 -0500

    made small adjustments

--- Scripts/Generation/Unpackers/AbstractBuildings/abstract_buildings_box_compact_unpacker.gd
+++ Scripts/Generation/Unpackers/AbstractBuildings/abstract_buildings_box_compact_unpacker.gd
@@ -2,6 +2,12 @@ extends Unpacker
 
 @export var models: Array[AbstractRoomTemplate]
 
+#Steps
+#1: Place main room
+#2: Place 1-2 hallways from main room (in different directions)
+#3: Attatch stairways to hallways and add hallways above hallways
+#4: Attatch smaller rooms to hallways and main room (Maybe even create another large room off of hallways or large room)
+
 func unpack() -> Array[Unpacker]:
 	var rng := RandomNumberGenerator.new()
 	rng.seed = seed
